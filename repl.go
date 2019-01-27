a81435d3a740564b629a5c71be35a87bc9739eaa
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-01-27 11:11:24 +0100

    repl: more control for repl/console widget

--- repl.go
+++ repl.go
@@ -3,7 +3,6 @@ package ui
 import (
 	"fmt"
 	"image"
-	"io"
 
 	"golang.org/x/mobile/event/key"
 )
@@ -12,26 +11,40 @@ import (
 type Repl struct {
 	Target **Repl
 	Edit
-	Interp Interpreter
+	Interp      Interpreter
+	Reply       bool // Append command to the Repl widget, if it is not on the last line.
+	Currentline bool // Use current line if nothing is selected.
 }
 
 type Interpreter interface {
-	Eval(string, io.Writer)
+	Eval(string)
 	Cancel()
 }
 
 // TODO: On ENTER execute current line if nothing is selected.
-// Otherwise execute selected (multiline) text.
+// Otherwise execute selected (possibly multiline) text.
 // TODO: cancel with ESC?
 func (r *Repl) Key(w *Window, self *Kid, k key.Event, m Mouse, orig image.Point) (res Result) {
 	res.Consumed = true
 	if k.Code == key.CodeReturnEnter {
 		if k.Direction != key.DirRelease {
-			fmt.Println("Eval...") // TODO
-			return res
+			if r.Interp == nil {
+				fmt.Println("Repl: Interpeter is nil")
+				return res
+			}
+			t := r.Edit.TextBox.Selection()
+			if t != "" {
+				r.Interp.Eval(t)
+				return res
+			}
+			if r.Currentline == false {
+				goto def
+			}
+			fmt.Println("TODO: repl: exec current line")
 		}
 		return res
 	}
+def:
 	e := r.Edit
 	e.mods = e.keyEvent(e.mods, k)
 	res.Consumed = true
