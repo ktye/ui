6ce77c44833de4d171e072e323c68c94d52a20d0
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2019-05-03 16:25:57 +0200

    add Blank

--- base/base.go
+++ base/base.go
@@ -53,12 +53,18 @@ func (k *Kid) Draw(dst *image.RGBA, force bool) {
 	k.Widget.Draw(dst.SubImage(k.Rectangle).(*image.RGBA), force)
 }
 
-/*
-func (k *Kid) Mouse(pos image.Point, but int, dir int, mod uint32) int {
-	return k.Widget.Mouse(pos.Sub(k.Rectangle.Min), but, dir, mod)
-}
-*/
-
 func clear(dst *image.RGBA) {
 	draw.Draw(dst, dst.Bounds(), Colors[1], image.ZP, draw.Src)
 }
+
+type Blank struct {
+	Base
+}
+
+func (b *Blank) Draw(dst *image.RGBA, force bool) {
+	if force {
+		clear(dst)
+	}
+}
+func (b *Blank) Mouse(pos image.Point, but int, dir int, mod uint32) int { return 0 }
+func (b *Blank) Key(r rune, code uint32, dir int, mod uint32) int        { return 0 }
